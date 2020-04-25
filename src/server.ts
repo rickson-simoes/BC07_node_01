2355878d0a5e3ff3ffdf61e92e67e59361a1020b
Rickson <rickson.simoes@hotmail.com>
2020-04-25 00:26:08 -0300

    :hammer: :shirt: :wrench: Configurando - ESLint, Prettier, Editorconfig

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.listen(3333, () => {
-  console.log('✌️ Server started on port 3333');
-});
+app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
-  return res.json({message: 'yay'});
-})
+app.listen(3333, () => {
+  console.log("✌️ Server started on port 3333");
+});
